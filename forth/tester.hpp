14576967ec3f954a094730da09e19a0d63f7f4fb
Lars Krueger <lars_e_krueger@gmx.de>
2016-10-19 19:17:34 +0200

    non-functional: add comments

--- forth/tester.hpp
+++ forth/tester.hpp
@@ -9,6 +9,8 @@
 
 namespace forth
 {
+  /** This class stores a list of test cases.
+   */
   class Tester
   {
     public:
@@ -26,10 +28,13 @@ namespace forth
 
       };
 
+      /** A test case defines input and output of a function call.
+       */
       class TestCase
       {
         public:
 
+          /// Construct a test case by name
           TestCase(
             const std::string &a_name)
             : m_name( a_name)
@@ -45,6 +50,7 @@ namespace forth
             return m_name;
           }
 
+          /// Set the line to call for this test case
           void
           SetStartLine(
             Runtime::Cell a_line)
@@ -52,12 +58,14 @@ namespace forth
             m_start_line = a_line;
           }
 
+          /// Get the line to call for this test case
           size_t
           GetStartLine() const
           {
             return m_start_line;
           }
 
+          /// Add a number to be used for input
           void
           AddInputNumber(
             Runtime::Cell a_nr)
@@ -65,6 +73,7 @@ namespace forth
             m_input_stack.push_back( a_nr);
           }
 
+          /// Add a number to be used for checking the output of this test case
           void
           AddOutputNumber(
             Runtime::Cell a_nr)
@@ -72,6 +81,7 @@ namespace forth
             m_output_stack.push_back( a_nr);
           }
 
+          /// Get the inputs of this test case
           const std::vector<Runtime::Cell> &
 
           GetInput() const
@@ -79,6 +89,7 @@ namespace forth
             return m_input_stack;
           }
 
+          /// Get the desired output of this test case
           const std::vector<Runtime::Cell> &
 
           GetOutput() const
@@ -87,21 +98,33 @@ namespace forth
           }
 
         protected:
+
+          /// Name of this test case
           std::string m_name;
+
+          /// Line to start for this test case
           Runtime::Cell m_start_line;
+
+          /// Values of the data stack to be used for the test
           std::vector<Runtime::Cell> m_input_stack;
+
+          /// Values of the data stack to be expected after the function call
           std::vector<Runtime::Cell> m_output_stack;
       };
 
+      /// Construct a tester
       Tester();
 
+      /// Parse a test case description file
       void
       ParseFromFile(
         const char * a_filename);
 
+      /// Return the number of test cases loaded
       size_t
       CountTestCases() const;
 
+      /// Access a specific test case
       const TestCase &
 
       GetTestCase(
@@ -109,19 +132,23 @@ namespace forth
 
     protected:
 
+      /// List of test cases
       std::vector<TestCase> m_test_case;
 
+      /// Parse a test case description file from a stream
       void
       ParseFromStream(
         const char *  a_filename,
         std::istream &a_input);
 
+      /// Helper function to process a single line of the test case file
       void
       ProcessLine(
         const char * a_filename,
         size_t a_lineNo,
         const std::string &a_line);
 
+      /// Throw a ParseError exception with a proper error message
       void
       ThrowParseError(
         const char * a_filename,
